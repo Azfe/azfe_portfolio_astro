015b6ee3c5c39eac68eb5a02a62728a0c4453a35
Alex Zapata <alexzapata1984@gmail.com>
2026-05-08 01:24:00 +0200

    fix: fixed link to cv.azfe.dev

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,7 @@ export const API_URL = import.meta.env.PUBLIC_API_URL as string;
 
 export const ROUTES = {
   home: "/",
-  cv: "/cv",
+  cv: "https://cv.azfe.dev/",
   projects: "/projects",
   notFound: "/404",
 } as const;
